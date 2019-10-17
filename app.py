9c8d053c1721961f3e72460739e77ffe36ee38ac
lehlohonolo@bluevinegroup.co.za <joseleh@live.com>
2019-10-17 15:33:38 +0200

    Added weight conversion

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ full_name = input('Enter Name: ')
 profession = input('What\'s your profession? ')
 birth_year = input('Enter your Birth year: ')
 age = now.year - int(birth_year)
+weight_in_lbs = input('Please enter your weight (lbs): ')
+weight = int(weight_in_lbs) * 0.45
 
 
-print('Hi ' + full_name + ', We love ' + profession + 's at the age ' + str(age) + ', Wow great!!!')
-print(age)
+print('Hi ' + full_name + ', We love ' + profession + 's at the age ' + str(age) + ' with ' + str(weight) +
+      'kg weight, Wow great!!!')
+
